79bdaab7679d67f8407411eebb0a833b3438a78d
Artur <cutscenedev@gmail.com>
2025-06-29 11:49:20 +0200

    setup MUI

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+import CssBaseline from '@mui/material/CssBaseline'
+import HomePage from '../pages/HomePage'
+
+export default function App() {
+  return (
+    <>
+      <CssBaseline />
+      <HomePage />
+    </>
+  )
+}
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+import Button from "@mui/material/Button";
+
+export default function HomePage() {
+  return (
+    <>
+      <Button variant="contained">Hello world</Button>
+    </>
+  )
+}
