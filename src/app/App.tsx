9b5e60d8914426cb205f0ff2ca7dfdda648d96dc
Artur <cutscenedev@gmail.com>
2025-06-29 21:36:32 +0200

    add global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,18 +1,19 @@
-import CssBaseline from '@mui/material/CssBaseline'
 import HomePage from '../pages/HomePage'
 import { AppConfigProvider } from './config/AppConfig'
 import { WeatherApiConnectorProvider } from './api/connectors/AppWeatherApiConnector'
 import { WeatherApiProviderProvider } from './api/providers/AppWeatherApiProvider'
+import AppStyles from './styles/AppStyles'
 
 export default function App() {
   return (
-    <AppConfigProvider>
-      <WeatherApiConnectorProvider>
-        <WeatherApiProviderProvider>
-          <CssBaseline />
-          <HomePage />
-        </WeatherApiProviderProvider>
-      </WeatherApiConnectorProvider>
-    </AppConfigProvider>
+    <AppStyles>
+      <AppConfigProvider>
+        <WeatherApiConnectorProvider>
+          <WeatherApiProviderProvider>
+            <HomePage />
+          </WeatherApiProviderProvider>
+        </WeatherApiConnectorProvider>
+      </AppConfigProvider>
+    </AppStyles>
   )
 }
---
+++ src/app/styles/AppStyles.tsx
@@ -0,0 +1,32 @@
+import { GlobalStyles, GlobalStylesProps } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
+import { PropsWithChildren } from "react";
+
+
+function AppStyles({ children }: PropsWithChildren) {
+  const globalStyles: GlobalStylesProps["styles"] = {
+    "height": "100%",
+    "width": "100%",
+  };
+
+  return (
+    <>
+      <GlobalStyles
+        styles={{
+          html: globalStyles,
+          body: globalStyles,
+          "div#root": {
+            ...globalStyles,
+            "display": "flex",
+            "flexDirection": "column",
+            "alignItems": "stretch",
+          },
+        }}
+      />
+      <CssBaseline enableColorScheme />
+      {children}
+    </>
+  );
+}
+
+export default AppStyles;
